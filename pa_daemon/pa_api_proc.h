75476ee6d218056e2ae275be8be0cc0e2ec0f287
marklion <marklion@sina.com>
2021-03-22 13:36:43 +0800

    delete dead code

--- pa_daemon/pa_api_proc.h
+++
@@ -1,8 +0,0 @@
-#if !defined(_PA_API_PROC_H_)
-#define _PA_API_PROC_H_
-#include "../tcp_framework/tdf_include.h"
-
-void pa_api_proc_new_connect(const std::string& _chrct);
-void pa_api_proc_hunup(const std::string &_chrct);
-void pa_api_proc_data(const std::string &_chrct, const std::string &_data);
-#endif // _PA_API_PROC_H_
