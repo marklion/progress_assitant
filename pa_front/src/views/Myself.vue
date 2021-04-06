767e189f8f8aa5e11a42c87c2861b2321fe0452a
marklion <marklion@sina.com>
2021-04-06 12:17:07 +0800

    change myself to mainstream style

--- pa_front/src/views/Myself.vue
+++ pa_front/src/views/Myself.vue
@@ -1,25 +1,38 @@
 <template>
 <div class="myself_show">
-    <van-cell-group>
-        <van-cell is-link :to="{name: 'SelfInfo'}" value="修改信息">
-            <template #title>
-                <div>
-                    <van-row align="center" type="flex">
-                        <van-col>
-                            <van-image round width="60px" height="60px" :src="$remote_url +  $store.state.userinfo.logo" />
-                        </van-col>
-                        <van-col>
-                            <div class="name_show">{{$store.state.userinfo.name}}</div>
-                            <div class="role_show">{{$store.state.userinfo.company}}</div>
-                        </van-col>
-                    </van-row>
-                </div>
-            </template>
-        </van-cell>
-        <van-cell v-if="is_admin" is-link :to="{name:'Admin'}" title="管理员菜单"></van-cell>
-    </van-cell-group>
-    <van-button v-if="$store.state.is_login" type="danger" @click="logoff" block>退出登录</van-button>
-    <van-button v-else type="primary" @click="login" block>登录</van-button>
+    <div v-if="$store.state.is_login">
+        <van-row type="flex" justify="center" align="center">
+            <van-image round width="80px" height="80px" :src="$remote_url +  $store.state.userinfo.logo" />
+        </van-row>
+        <van-row type="flex" justify="center" align="center">
+            <van-col>
+                <h3>{{$store.state.userinfo.name}}</h3>
+            </van-col>
+            <van-col>
+                <van-button icon="edit" round size="small" :to="{name: 'SelfInfo'}" />
+            </van-col>
+        </van-row>
+        <van-row type="flex" justify="center" align="center">
+            <div class="role_show">{{$store.state.userinfo.company}}</div>
+        </van-row>
+        <van-divider />
+        <van-cell-group>
+            <van-cell v-if="is_admin" icon="manager-o" is-link :to="{name:'Admin'}" title="管理员菜单"></van-cell>
+        </van-cell-group>
+        <van-divider />
+        <van-button type="danger" @click="logoff" block>退出登录</van-button>
+    </div>
+    <div v-else class="unlogin_show">
+        <van-row type="flex" justify="center" align="center">
+            <van-image round width="80px" height="80px" :src="$remote_url +  '/logo_res/logo_1617265618.jpg'" />
+        </van-row>
+        <van-row type="flex" justify="center" align="center">
+            <h1>掌易助理</h1>
+        </van-row>
+        <div style="margin: 16px;">
+            <van-button type="primary" @click="login" block>微信登录</van-button>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -37,7 +50,15 @@ import {
 import {
     Button
 } from 'vant';
+import {
+    Icon
+} from 'vant';
+import {
+    Divider
+} from 'vant';
 
+Vue.use(Divider);
+Vue.use(Icon);
 Vue.use(Button);
 Vue.use(Cell);
 Vue.use(CellGroup);
@@ -74,6 +95,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.unlogin_show {
+    margin-top:100px;
+}
+</style>>
 
-</style>
