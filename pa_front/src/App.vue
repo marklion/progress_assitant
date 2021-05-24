64dd0952363642bfecce4422cadf7aebd68155e3
marklion <marklion@sina.com>
2021-05-24 15:02:18 +0800

    ZYZL-39 #comment add more placeholder

--- pa_front/src/App.vue
+++ pa_front/src/App.vue
@@ -8,7 +8,7 @@
     <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
         <router-view />
     </van-pull-refresh>
-    <div style="height: 4rem;"></div>
+    <div style="height: 6rem;"></div>
     <van-tabbar route fixed>
         <van-tabbar-item v-if="$store.state.userinfo.buyer" replace :to="{name:'Home'}" icon="home-o">主页</van-tabbar-item>
         <van-tabbar-item v-else replace :to="{name:'CompanyHome'}" icon="home-o">主页</van-tabbar-item>
