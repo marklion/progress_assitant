f107b889d766684605ca839f9b64ff18b944170e
marklion <marklion@sina.com>
2021-06-16 12:15:08 +0800

    ZYZL-65 #comment add quick add button when no contract found

--- pa_front/src/components/ContractCell.vue
+++ pa_front/src/components/ContractCell.vue
@@ -9,11 +9,25 @@
                 </van-col>
             </van-row>
         </template>
+        <template #right-icon v-if="!contract.number && !$store.state.userinfo.buyer">
+            <van-button type="primary" size="small" @click="add_contract_show = true">增加</van-button>
+        </template>
         <div v-if="contract.number">
             <div>开始日期：{{contract.start_time}}</div>
             <div>到期日期：{{contract.end_time}}</div>
         </div>
     </van-cell>
+    <van-dialog v-model="add_contract_show" title="添加合同" :showConfirmButton="false" closeOnClickOverlay>
+        <van-form @submit="add_contract">
+            <van-field v-model="submit_contract.a_side_company" name="甲方" label="甲方" placeholder="请输入甲方公司名" :rules="[{ required:true, message:'请输入甲方公司'}]" />
+            <van-field v-model="submit_contract.number" name="编号" label="合同编号" placeholder="请输入合同编号" :rules="[{ required:true, message:'请输入合同编号'}]" />
+            <van-field name="calendar1" v-model="submit_contract.start_time" label="开始日期" placeholder="请输入开始日期yyyy/mm/dd" format-trigger="onBlur" :formatter="formatter_input_date" :rules="[{ required:true, message:'请输入开始日期'}]" />
+            <van-field name="calendar2" v-model="submit_contract.end_time" label="到期日期" placeholder="请输入到期日期yyyy/mm/dd" format-trigger="onBlur" :formatter="formatter_input_date" :rules="[{ required:true, message:'请输入到期日期'}]" />
+            <div style="margin: 16px;">
+                <van-button round block type="info" native-type="submit">提交</van-button>
+            </div>
+        </van-form>
+    </van-dialog>
 </div>
 </template>
 
@@ -30,7 +44,23 @@ import {
     Col,
     Row
 } from 'vant';
+import {
+    Form
+} from 'vant';
+import {
+    Field
+} from 'vant';
+import {
+    Dialog
+} from 'vant';
+import {
+    Button
+} from 'vant';
 
+Vue.use(Button);
+Vue.use(Dialog);
+Vue.use(Form);
+Vue.use(Field);
 Vue.use(Col);
 Vue.use(Row);
 Vue.use(Tag);
@@ -44,6 +74,13 @@ export default {
     },
     data: function () {
         return {
+            submit_contract: {
+                number: '',
+                a_side_company: '',
+                start_time: '',
+                end_time: '',
+            },
+            add_contract_show: false,
             contract: {
                 start_time: '',
                 end_time: '',
@@ -66,11 +103,34 @@ export default {
         };
     },
     methods: {
+        add_contract: function () {
+            var vue_this = this;
+            vue_this.$call_remote_process("company_management", "add_contract", [vue_this.$cookies.get('pa_ssid'), vue_this.submit_contract]).then(function (resp) {
+                if (resp) {
+                    vue_this.get_contract(vue_this.a_side, vue_this.b_side);
+                    vue_this.add_contract_show = false;
+                }
+            });
+        },
         get_contract: function (_a_side, _b_side) {
             var vue_this = this;
             vue_this.$call_remote_process_no_toast("company_management", "get_contract", [_a_side, _b_side]).then(function (resp) {
                 vue_this.contract = resp;
             });
+        },
+        formatter_input_date: function (_value) {
+            if (_value.length <= 0) {
+                return _value;
+            }
+            return this.formatDateTime(new Date(_value));
+        },
+        formatDateTime: function (date) {
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            m = m < 10 ? ('0' + m) : m;
+            var d = date.getDate();
+            d = d < 10 ? ('0' + d) : d;
+            return y + '-' + m + '-' + d;
         },
     },
     watch: {
@@ -83,6 +143,8 @@ export default {
     },
     beforeMount: function () {
         this.get_contract(this.a_side, this.b_side);
+
+        this.submit_contract.a_side_company = this.a_side;
     }
 
 }
