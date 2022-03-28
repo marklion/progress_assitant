32865651a0e76eef5cfc8cebecba56faa8ee3dc5
Dong Chen <dongchen0502@163.com>
2022-03-28 12:52:51 +0800

    ZYZL-212 #update datetimePicker logic

--- pa_front/src/api/plan.js
+++ pa_front/src/api/plan.js
@@ -6,7 +6,7 @@ import {
  * @param {String} plan_id
  * @return {Promise<PlanDetail>}
  */
-export async function getPlanInfo(plan_id){
-    return await call_remote_process("stuff_plan_management", "get_plan",
+export function getPlanInfo(plan_id){
+    return call_remote_process("stuff_plan_management", "get_plan",
         [plan_id]);
 }
