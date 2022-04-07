86467a8644bbbc722451dd5ee769d26eb55b5da2
Dong Chen <dongchen0502@163.com>
2022-04-07 23:30:35 +0800

    ZYZL-231 #fix ZYZL-232

--- pa_front/src/api/stuff.js
+++ pa_front/src/api/stuff.js
@@ -31,11 +31,8 @@ export function getAllBidding(ssid, status_condition = -1){
  * @return {Promise<bidding_params>}
  */
 export async function getBiddingById(ssid, biddingId){
-    // return call_remote_process("stuff_info", "get_bidding",
-    //     [ssid, biddingId]);
-    let list = await call_remote_process("stuff_info", "get_all_bidding",
-        [ssid, -1]);
-    return list.find(item => item.id == biddingId)
+    return call_remote_process("stuff_info", "get_bidding",
+        [ssid, +biddingId]);
 }
 
 /**
